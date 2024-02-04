036664fccfd1af1e65c3a417e8c4b10cec3809c9
kotyba-km <kotybaalkassarkm@gmail.com>
2024-02-04 18:28:06 +0200

    add color mode

--- script.js
+++ script.js
@@ -26,3 +26,42 @@ menu_btn.addEventListener('click',function() {
 close_menu.addEventListener('click' ,function(){
   menu.classList.remove('is-active')
 })
+const colorMode1 = document.querySelector('.color-button1');
+const navy = document.querySelector('.navy');
+const startSection = document.querySelectorAll('.start');
+const menuOption = document.querySelectorAll('.social-menu a');
+const heroSection = document.querySelector('.hero-section');
+const webHolder = document.querySelectorAll('.web-holder');
+const text = document.querySelectorAll('.text');
+function changeToDark() {
+  menu.style.background="#151517";
+  navy.style.background="linear-gradient(90deg, rgba(21,21,23,1) 0%, rgba(40,38,38,1) 50%, rgba(21,21,23,1) 100%)";
+  heroSection.style.background="#151517";
+  heroSection.style.border="10px solid gray";
+  for (let i = 0; i < webHolder.length; i++) {
+    webHolder[i].style.backgroundColor = "#2a2a2d";
+  }
+  for (let i = 0; i < text.length; i++) {
+    text[i].style.backgroundColor = "#212121";
+  }
+  for (let i = 0; i < startSection.length; i++) {
+    startSection[i].style.backgroundColor = "#151517";
+    startSection[i].style.border="10px solid gray";
+  }
+  for (let i = 0; i < menuOption.length; i++) {
+    menuOption[i].style.backgroundColor = "#2a2a2d";
+  }
+}
+function changeToBlue() {
+  menu.style.background="#000634";
+  navy.style.background="linear-gradient(45deg, rgba(27,0,140,1) 0%, rgba(5,0,25,1) 100%)";
+  heroSection.style.background="#000832";
+  heroSection.style.border="10px solid #00126d";
+  for (let i = 0; i < startSection.length; i++) {
+    startSection[i].style.backgroundColor = "#000832";
+    startSection[i].style.border="10px solid #00126d";
+  }
+  for (let i = 0; i < menuOption.length; i++) {
+    menuOption[i].style.backgroundColor = "#000a58";
+  }
+}
